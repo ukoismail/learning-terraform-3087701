d335dce56634e3222d4cc0a2fa98297c3e233dbb
ukoismail <67784891+ukoismail@users.noreply.github.com>
2024-02-17 01:38:06 +0500

    autoscalling

--- main.tf
+++ main.tf
@@ -34,17 +34,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  
-  subnet_id = module.blog_vpc.public_subnets[0]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns 
+  security_groups     = [module.blog_sg.security_group_id]
+  
+  image_id               = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
+
 }
+
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
