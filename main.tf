dd9c8ce3fd5dd8eeaaec4d25ff0f8c3488670c77
ukoismail <67784891+ukoismail@users.noreply.github.com>
2024-02-17 01:27:27 +0500

    copied

--- main.tf
+++ main.tf
@@ -45,33 +45,34 @@ resource "aws_instance" "blog" {
     Name = "Learning Terraform"
   }
 }
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
+  name = "blog-alb"
 
-   vpc_security_group_ids = [module.blog_sg.security_group_id]
+  load_balancer_type = "application"
 
-  listeners = {
-    ex-http = {
-      port            = 80
-      protocol        = "HTTP"
-      forward = {
-        target_group_key = aws_instance.blog.id
-      }
-    }
-  }
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-  target_groups = {
-    aws_instance.blog.id = {
+  target_groups = [
+    {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
     }
-  }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
